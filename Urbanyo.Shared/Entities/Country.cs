6be86a2c2f98e7eeed630b1d094b655b5ffdd2d5
ESTIVEN ALONSO SEGURO ACOSTA <estivenseguro229799@correo.itm.edu.co>
2023-04-15 13:57:11 -0500

    Add InitialDb and DataContext

---
+++ Urbanyo.Shared/Entities/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Urbanyo.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "país")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "el campo {0} no puede tener mas de {1} caractéres.")]
+        public string Name { get; set; } = null!;
+    }
+}
