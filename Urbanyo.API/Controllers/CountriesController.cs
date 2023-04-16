87893f4dc877e579cbc490176a40b7b3cc11e0f3
ESTIVEN ALONSO SEGURO ACOSTA <estivenseguro229799@correo.itm.edu.co>
2023-04-15 22:38:06 -0500

    Complete countries API controller

--- Urbanyo.API/Controllers/CountriesController.cs
+++ Urbanyo.API/Controllers/CountriesController.cs
@@ -17,18 +17,54 @@ namespace Urbanyo.API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult> Get()
+        public async Task<ActionResult> GetAsync()
         {
             return Ok(await _context.Countries.ToListAsync());
         }
 
+        [HttpGet("{id:int}")]
+
+        public async Task<ActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(country);
+        }
+
+
         [HttpPost]
-        public async Task<ActionResult> Post(Country country)
+        public async Task<ActionResult> PostAsync(Country country)
         {
             _context.Add(country);
             await _context.SaveChangesAsync();
             return Ok(country);
         }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+           var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
     }
 }
 
