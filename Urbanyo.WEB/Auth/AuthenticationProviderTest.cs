ad49d985f0f693992a44f153fc8a41f0cd427c24
ESTIVEN ALONSO SEGURO ACOSTA <estivenseguro229799@correo.itm.edu.co>
2023-04-16 23:22:57 -0500

    security hardcode on WEB

---
+++ Urbanyo.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Urbanyo.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var EstivenUser = new ClaimsIdentity(new List<Claim>
+                {
+                      new Claim("FirstName", "Estiven"),
+                      new Claim("LastName", "Seguro"),
+                      new Claim(ClaimTypes.Name, "stiven@yopmail.com"),
+                      new Claim(ClaimTypes.Role, "Admin")
+                },
+            authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+            //anonumous decrementa el contador y zuluUser lo aumenta y permite ver los paises
+        }
+    }
+}
+
