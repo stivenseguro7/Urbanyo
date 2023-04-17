251684b905d25635c6312c7bb5f69612d30d14c7
ESTIVEN ALONSO SEGURO ACOSTA <estivenseguro229799@correo.itm.edu.co>
2023-04-17 01:00:06 -0500

    Cascade Drop Down list in register

--- Urbanyo.API/Controllers/StatesController.cs
+++ Urbanyo.API/Controllers/StatesController.cs
@@ -23,6 +23,16 @@ namespace Urbanyo.API.Controllers
             _context = context;
         }
 
+        [AllowAnonymous]
+        [HttpGet("combo/{countryId:int}")]
+        public async Task<ActionResult> GetCombo(int countryId)
+        {
+            return Ok(await _context.States
+                .Where(x => x.CountryId == countryId)
+                .ToListAsync());
+
+        }
+
         [HttpGet]
         public async Task<ActionResult> Get([FromQuery] PaginationDTO pagination)
         {
