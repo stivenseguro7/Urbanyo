78c27a3c4be44033bf90811ac2a34f17d42e8352
MATEO HERRERA GUTIERREZ <mateoherrera264627@correo.itm.edu.co>
2023-04-25 10:11:26 -0500

    add questions class

---
+++ Urbanyo.Shared/Entities/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urbanyo.Shared.Entities
+{
+    internal class Question
+    {
+        public string Text { get; set; }
+        public List<string> Choices { get; set; }
+        public int Answer { get; set; }
+        public List<int> Amount { get; set; }
+        public List<string> Archivos { get; set; }
+        public string Text1 { get; set; }
+    }
+}
